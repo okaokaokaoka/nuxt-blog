13fcf28a010df9b5ebe54e535cdcc540e82b2d31
岡田徹 <toruhandbell0424@gmail.com>
2019-01-13 01:42:08 +0900

    Add markdown-vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -17,39 +17,18 @@ html {
   box-sizing: border-box;
 }
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
+.slide-left-enter {
+  transform: translateX(2000px);
+  opacity: 0;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+.slide-left-enter-active {
+  transition: all 0.3s linear;
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+.slide-left-leave-to {
+  transform: translateX(-2000px);
+  opacity: 0;
 }
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.slide-left-leave-active {
+  transition: all 0.3s linear;
 }
 </style>
